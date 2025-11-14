c482548159fb69370fa3a6328786c77145d35b0f
EduardoLMartins <eduardol_martins@hotmail.com>
2025-11-14 09:48:51 -0300

    Separa código por metódos e Aplica SOLID - SRP

--- cotuba/src/main/java/cotuba/cli/LeitorOpcoesCLI.java
+++ cotuba/src/main/java/cotuba/cli/LeitorOpcoesCLI.java
@@ -15,7 +15,6 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
 
-
 public class LeitorOpcoesCLI {
 
 	// cotuba.LeitorOpcoesCLI
@@ -24,84 +23,114 @@ public class LeitorOpcoesCLI {
 	private Path arquivoDeSaida;
 	private boolean modoVerboso = false;
 
+	/*
+	 * Este construtor nao precisa ser publico pois só é utilizado dentro do pacote
+	 * cli sendo assim deixando ele com protect Nao a como reutilizado de forma
+	 * indevida em outras classes
+	 */
 	LeitorOpcoesCLI(String[] args) {
-		
-		 var options = new Options();
-
-		    var opcaoDeDiretorioDosMD = new Option("d", "dir", true,
-		        "Diretório que contém os arquivos md. Default: diretório atual.");
-		    options.addOption(opcaoDeDiretorioDosMD);
-
-		    var opcaoDeFormatoDoEbook = new Option("f", "format", true,
-		        "Formato de saída do ebook. Pode ser: pdf ou epub. Default: pdf");
-		    options.addOption(opcaoDeFormatoDoEbook);
-
-		    var opcaoDeArquivoDeSaida = new Option("o", "output", true,
-		        "Arquivo de saída do ebook. Default: book.{formato}.");
-		    options.addOption(opcaoDeArquivoDeSaida);
-
-		    var opcaoModoVerboso = new Option("v", "verbose", false,
-		        "Habilita modo verboso.");
-		    options.addOption(opcaoModoVerboso);
-
-		    CommandLineParser cmdParser = new DefaultParser();
-		    var ajuda = new HelpFormatter();
-		    CommandLine cmd;
-
-		    try {
-		      cmd = cmdParser.parse(options, args);
-		    } catch (ParseException e) {
-		      ajuda.printHelp("cotuba", options);
-
-		      throw new IllegalArgumentException("Opção invalida", e); 
-		    }
-		    try {
-
-		        String nomeDoDiretorioDosMD = cmd.getOptionValue("dir");
-
-		        if (nomeDoDiretorioDosMD != null) {
-		          diretorioDosMD = Paths.get(nomeDoDiretorioDosMD);
-		          if (!Files.isDirectory(diretorioDosMD)) {
-		            throw new IllegalArgumentException(nomeDoDiretorioDosMD + " não é um diretório.");
-		          }
-		        } else {
-		          Path diretorioAtual = Paths.get("");
-		          diretorioDosMD = diretorioAtual;
-		        }
-
-		        String nomeDoFormatoDoEbook = cmd.getOptionValue("format");
-
-		        if (nomeDoFormatoDoEbook != null) {
-		          formato = nomeDoFormatoDoEbook.toLowerCase();
-		        } else {
-		          formato = "pdf";
-		        }
-
-		        String nomeDoArquivoDeSaidaDoEbook = cmd.getOptionValue("output");
-		        if (nomeDoArquivoDeSaidaDoEbook != null) {
-		          arquivoDeSaida = Paths.get(nomeDoArquivoDeSaidaDoEbook);
-		        } else {
-		          arquivoDeSaida = Paths.get("book." + formato.toLowerCase());
-		        }
-		        if (Files.isDirectory(arquivoDeSaida)) {
-		          // deleta arquivos do diretório recursivamente
-		          Files.walk(arquivoDeSaida).sorted(Comparator.reverseOrder())
-		              .map(Path::toFile).forEach(File::delete);
-		        } else {
-		          Files.deleteIfExists(arquivoDeSaida);
-		        }
-
-		        modoVerboso = cmd.hasOption("verbose");
-
-	}
-	 catch (IOException ex) {
-		 throw new IllegalArgumentException(ex);
-	 }
+		Options options = criarOpcoes();
+		CommandLine cmd = parseDosArgumentos(args, options);
+		trataDiretoriosDosMD(cmd);
+		trataFormato(cmd);
+		trataArquivoDeSaida(cmd);
+		trataModoVerboso(cmd);
 	}
 
-	
+	private Options criarOpcoes() {
+		var options = new Options();
+
+		var opcaoDeDiretorioDosMD = new Option("d", "dir", true,
+				"Diretório que contém os arquivos md. Default: diretório atual.");
+		options.addOption(opcaoDeDiretorioDosMD);
+
+		var opcaoDeFormatoDoEbook = new Option("f", "format", true,
+				"Formato de saída do ebook. Pode ser: pdf ou epub. Default: pdf");
+		options.addOption(opcaoDeFormatoDoEbook);
+
+		var opcaoDeArquivoDeSaida = new Option("o", "output", true,
+				"Arquivo de saída do ebook. Default: book.{formato}.");
+		options.addOption(opcaoDeArquivoDeSaida);
+
+		var opcaoModoVerboso = new Option("v", "verbose", false, "Habilita modo verboso.");
+		options.addOption(opcaoModoVerboso);
+
+		return options;
+	}
+
+	private CommandLine parseDosArgumentos(String[] args, Options options) {
+		CommandLineParser cmdParser = new DefaultParser();
+		var ajuda = new HelpFormatter();
+		CommandLine cmd;
+
+		try {
+			cmd = cmdParser.parse(options, args);
+		} catch (ParseException e) {
+			ajuda.printHelp("cotuba", options);
+
+			throw new IllegalArgumentException("Opção invalida", e);
+		}
+
+		return cmd;
+
+	}
+
+	private void trataDiretoriosDosMD(CommandLine cmd) {
+
+		String nomeDoDiretorioDosMD = cmd.getOptionValue("dir");
+
+		if (nomeDoDiretorioDosMD != null) {
+			diretorioDosMD = Paths.get(nomeDoDiretorioDosMD);
+			if (!Files.isDirectory(diretorioDosMD)) {
+				throw new IllegalArgumentException(nomeDoDiretorioDosMD + " não é um diretório.");
+			}
+		} else {
+			Path diretorioAtual = Paths.get("");
+			diretorioDosMD = diretorioAtual;
+		}
+	}
+
+	private void trataFormato(CommandLine cmd) {
+
+		String nomeDoFormatoDoEbook = cmd.getOptionValue("format");
+
+		if (nomeDoFormatoDoEbook != null) {
+			formato = nomeDoFormatoDoEbook.toLowerCase();
+		} else {
+			formato = "pdf";
+		}
+
+	}
+
+	private void trataArquivoDeSaida(CommandLine cmd) {
+		try {
+
+			String nomeDoArquivoDeSaidaDoEbook = cmd.getOptionValue("output");
+			if (nomeDoArquivoDeSaidaDoEbook != null) {
+				arquivoDeSaida = Paths.get(nomeDoArquivoDeSaidaDoEbook);
+			} else {
+				arquivoDeSaida = Paths.get("book." + formato.toLowerCase());
+			}
+
+			if (Files.isDirectory(arquivoDeSaida)) {
+				// deleta arquivos do diretório recursivamente
+				Files.walk(arquivoDeSaida).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach(File::delete);
+			} else {
+				Files.deleteIfExists(arquivoDeSaida);
+			}
+		} catch (IOException ex) {
+			throw new IllegalArgumentException(ex);
+		}
+	}
+
+	private void trataModoVerboso(CommandLine cmd) {
+
+		modoVerboso = cmd.hasOption("verbose");
+
+	}
+
 	// getters e setters
-	
+
 	public Path getDiretorioDosMD() {
 		return diretorioDosMD;
 	}
@@ -116,6 +145,6 @@ public class LeitorOpcoesCLI {
 
 	public boolean isModoVerboso() {
 		return modoVerboso;
-	} 
-	
+	}
+
 }
--- cotuba/src/main/java/cotuba/md/RenderizadorMDParaHTML.java
+++ cotuba/src/main/java/cotuba/md/RenderizadorMDParaHTML.java
@@ -25,64 +25,76 @@ import cotuba.domain.Capitulo;
 public class RenderizadorMDParaHTML {
 
 	public List<Capitulo> renderiza(Path diretorioDosMD) {
-
-		List<Capitulo> capitulos = new ArrayList<Capitulo>();
-
-		PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
-
-		try (Stream<Path> arquivosMD = Files.walk(diretorioDosMD)) {
+		return obtemArquivosMD(diretorioDosMD).stream().map(arquivoMD -> {
+			Capitulo capitulo = new Capitulo();
+			Node  document = parseDoMD(arquivoMD, capitulo);
+			renderizaParaHTML(arquivoMD, capitulo, document);
 			
-			// TODO: voltar a pagina 56 para ajustar essa classe como exercicio extra
+			return capitulo;
 			
-			arquivosMD.filter(Files::isRegularFile).filter(p -> {
-				boolean match = matcher.matches(p.toAbsolutePath().normalize());
-				return match;
-			})
+		}).toList();
 
-					.sorted().forEach(arquivoMD -> {
 
-						Capitulo capitulo = new Capitulo();
-						Parser parser = Parser.builder().build();
-						Node document = null;
-						try {
-							document = parser.parseReader(Files.newBufferedReader(arquivoMD));
-							document.accept(new AbstractVisitor() {
-								@Override
-								public void visit(Heading heading) {
-									if (heading.getLevel() == 1) {
-										// capítulo
-										String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
-										capitulo.setTitulo(tituloDoCapitulo);
-									} else if (heading.getLevel() == 2) {
-										// seção
-									} else if (heading.getLevel() == 3) {
-										// título
-									}
-								}
+	}
 
-							});
-						} catch (Exception ex) {
-							throw new IllegalStateException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
-						}
+	private List<Path> obtemArquivosMD(Path diretorioDosMD) {
 
-						try {
-							HtmlRenderer renderer = HtmlRenderer.builder().build();
-							String html = renderer.render(document);
+	    PathMatcher matcher = FileSystems.getDefault().getPathMatcher("glob:**/*.md");
 
-							capitulo.setConteudoHTML(html);
-							capitulos.add(capitulo);
+	    try (Stream<Path> arquivosMD = Files.walk(diretorioDosMD)) {
 
-						} catch (Exception ex) {
-							throw new IllegalStateException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
-						}
+	        List<Path> lista = arquivosMD
+	                .filter(Files::isRegularFile)
+	                .filter(p -> matcher.matches(p.toAbsolutePath().normalize()))
+	                .sorted() // opcional, mas deixa capítulos em ordem alfabética
+	                .toList();
 
-					});
-		} catch (IOException ex) {
-			throw new IllegalStateException(
-					"Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
+	        return lista;
+
+	    } catch (IOException ex) {
+	        throw new IllegalStateException(
+	            "Erro tentando encontrar arquivos .md em " + diretorioDosMD.toAbsolutePath(), ex);
+	    }
+	}
+
+
+	private Node parseDoMD(Path arquivoMD, Capitulo capitulo) {
+		try {
+			Node document = null;
+			Parser parser = Parser.builder().build();
+
+			document = parser.parseReader(Files.newBufferedReader(arquivoMD));
+			document.accept(new AbstractVisitor() {
+				@Override
+				public void visit(Heading heading) {
+					if (heading.getLevel() == 1) {
+						// capítulo
+						String tituloDoCapitulo = ((Text) heading.getFirstChild()).getLiteral();
+						capitulo.setTitulo(tituloDoCapitulo);
+					} else if (heading.getLevel() == 2) {
+						// seção
+					} else if (heading.getLevel() == 3) {
+						// título
+					}
+				}
+
+			});
+			return document;
+		} catch (Exception ex) {
+			throw new IllegalStateException("Erro ao fazer parse do arquivo " + arquivoMD, ex);
 		}
 
-		return capitulos;
+	}
 
+	private void renderizaParaHTML(Path arquivoMD, Capitulo capitulo, Node document) {
+		try {
+			HtmlRenderer renderer = HtmlRenderer.builder().build();
+			String html = renderer.render(document);
+
+			capitulo.setConteudoHTML(html);
+
+		} catch (Exception ex) {
+			throw new IllegalStateException("Erro ao renderizar para HTML o arquivo " + arquivoMD, ex);
+		}
 	}
 }
